2a68fe8553f8774868dc313c499c03b044674a93
IsaacR <isaacrevelo55@gmail.com>
2024-02-28 17:27:13 -0500

    api para buscar

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -4,6 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\User;
+use App\Models\Occupation;
+use App\Models\Language;
+use App\Models\SelectionStatus;
+use App\Models\Vacancy;
+
 
 class ApiController extends Controller
 {
@@ -16,6 +22,41 @@ class ApiController extends Controller
             return response()->json(['mensaje' => 'Registro insertado correctamente'], 201);
         } catch (\Exception $e) {
             return response()->json(['mensaje' => 'Error al insertar el registro: ' . $e->getMessage()], 500);
+        }
+    }
+
+    public function users(Request $request){
+        $users= User::all();
+        return response()->json($users);
+    }
+
+    public function languages(Request $request){
+        $languages= Language::all();
+        return response()->json($languages);
+    }
+
+    public function occupations(Request $request){
+        $occupations= Occupation::all();
+        return response()->json($occupations);
+    }
+
+    public function selectionstatus(Request $request){
+        $selectionstatus= SelectionStatus::all();
+        return response()->json($selectionstatus);
+    }
+
+    public function vacancies(Request $request){
+        $vacancies= Vacancy::all();
+        return response()->json($vacancies);
+    }
+
+    public function searchUser(Request $request, $user){
+
+        try {
+            $search=User::findOrFail($user);
+            return response()->json($search);
+        } catch (\Exception $e) {
+            return response()->json('Error al buscar');
         }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ApiController;
 
 Route::post('/insertar_registro/{tabla}', [ApiController::class, 'insertarRegistro']);
+Route::get('/search_user/{user}', [ApiController::class, 'searchUser']);
+Route::get('/search_occupations/{}', [ApiController::class, 'search']);
+Route::get('/search_languages/{}', [ApiController::class, 'search']);
+Route::get('/search_vacancies/{}', [ApiController::class, 'search']);
+Route::get('/search_selectionstatus/{}', [ApiController::class, 'search']);
