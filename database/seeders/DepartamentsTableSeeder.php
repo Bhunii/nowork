5dc7d3e544f7ffaee65b8c48f2da138f4cf52e87
IsaacR <isaacrevelo55@gmail.com>
2024-03-08 15:50:56 -0500

    Seeders de departaments y municipalities

---
+++ database/seeders/DepartamentsTableSeeder.php
@@ -0,0 +1,183 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Departament;
+
+class DepartamentsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $departament1= New Departament;
+        $departament1->code='05';
+        $departament1->name='ANTIOQUIA';
+        $departament1->save();
+
+        $departament2= New Departament;
+        $departament2->code='15';
+        $departament2->name='BOYACA';
+        $departament2->save();
+
+        $departament3= New Departament;
+        $departament3->code='17';
+        $departament3->name='CALDAS';
+        $departament3->save();
+
+        $departament4= New Departament;
+        $departament4->code='18';
+        $departament4->name='CAQUETA';
+        $departament4->save();
+
+        $departament5= New Departament;
+        $departament5->code='19';
+        $departament5->name='CAUCA';
+        $departament5->save();
+
+        $departament6= New Departament;
+        $departament6->code='20';
+        $departament6->name='CESAR';
+        $departament6->save();
+
+        $departament7= New Departament;
+        $departament7->code='91';
+        $departament7->name='AMAZONAS';
+        $departament7->save();
+
+        $departament8= New Departament;
+        $departament8->code='81';
+        $departament8->name='ARAUCA';
+        $departament8->save();
+
+        $departament9= New Departament;
+        $departament9->code='13';
+        $departament9->name='BOLIVAR';
+        $departament9->save();
+
+        $departament10= New Departament;
+        $departament10->code='11';
+        $departament10->name='BOGOTA D.C';
+        $departament10->save();
+
+        $departament11= New Departament;
+        $departament11->code='85';
+        $departament11->name='CASANARE';
+        $departament11->save();
+
+        $departament12= New Departament;
+        $departament12->code='27';
+        $departament12->name='CHOCO';
+        $departament12->save();
+
+        $departament13= New Departament;
+        $departament13->code='23';
+        $departament13->name='CORDOBA';
+        $departament13->save();
+
+        $departament14= New Departament;
+        $departament14->code='25';
+        $departament14->name='CUNDINAMARCA';
+        $departament14->save();
+
+        $departament15= New Departament;
+        $departament15->code='94';
+        $departament15->name='GUAINIA';
+        $departament15->save();
+
+        $departament16= New Departament;
+        $departament16->code='95';
+        $departament16->name='GUAVIARE';
+        $departament16->save();
+
+        $departament17= New Departament;
+        $departament17->code='41';
+        $departament17->name='HUILA';
+        $departament17->save();
+
+        $departament18= New Departament;
+        $departament18->code='44';
+        $departament18->name='LA GUAJIRA';
+        $departament18->save();
+
+        $departament19= New Departament;
+        $departament19->code='47';
+        $departament19->name='MAGDALENA';
+        $departament19->save();
+
+        $departament20= New Departament;
+        $departament20->code='50';
+        $departament20->name='META';
+        $departament20->save();
+
+        $departament21= New Departament;
+        $departament21->code='52';
+        $departament21->name='NARIÑO';
+        $departament21->save();
+
+        $departament22= New Departament;
+        $departament22->code='54';
+        $departament22->name='NORTE DE SANTANDER';
+        $departament22->save();
+
+        $departament23= New Departament;
+        $departament23->code='86';
+        $departament23->name='PUTUMAYO';
+        $departament23->save();
+
+        $departament24= New Departament;
+        $departament24->code='63';
+        $departament24->name='QUINDIO';
+        $departament24->save();
+
+        $departament25= New Departament;
+        $departament25->code='66';
+        $departament25->name='RISARALDA';
+        $departament25->save();
+
+        $departament26= New Departament;
+        $departament26->code='88';
+        $departament26->name='SAN ANDRES';
+        $departament26->save();
+
+        $departament27= New Departament;
+        $departament27->code='68';
+        $departament27->name='SANTANDER';
+        $departament27->save();
+
+        $departament28= New Departament;
+        $departament28->code='70';
+        $departament28->name='SUCRE';
+        $departament28->save();
+
+        $departament29= New Departament;
+        $departament29->code='73';
+        $departament29->name='TOLIMA';
+        $departament29->save();
+
+        $departament30= New Departament;
+        $departament30->code='76';
+        $departament30->name='VALLE DEL CAUCA';
+        $departament30->save();
+
+        $departament31= New Departament;
+        $departament31->code='97';
+        $departament31->name='VAUPES';
+        $departament31->save();
+
+        $departament32= New Departament;
+        $departament32->code='99';
+        $departament32->name='VICHADA';
+        $departament32->save();
+
+        $departament33= New Departament;
+        $departament33->code='';
+        $departament33->name='';
+        $departament33->save();
+    }
+}
